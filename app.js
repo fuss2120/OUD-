d5d72efe65c356ae2a56bed2363182aa4ba9bf03
Scott Coyne <scoyne@tythonus.mscsnet.mu.edu>
2020-02-03 13:43:02 -0600

    serve static content at endpoints

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import path from 'path';
 
 const app = express()
 
+const static_dir = path.resolve('static_content') + '/';
+
 app.use(express.json())
+app.use(express.static('static_content'));
+
+app.get('/test', (req, res) => {
+  return res.status(200).send({'message': 'YAY! Congratulations! Your test is working'});
+})
 
 app.get('/', (req, res) => {
-  return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
+  res.sendFile(static_dir + 'index.html');
+})
+
+app.get('/entry', (req, res) => {
+  res.sendFile(static_dir + 'entry.html');
 })
 
 app.listen(3000);
